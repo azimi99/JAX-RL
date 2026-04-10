aaf54d06bbdecfed71b1557ab0574f56e39278f6
Alireza Azimi <alirezaazimi@Alirezas-MBP-3.lan>
2026-04-10 17:52:16 -0600

    fix video logging

--- DQN/src/utils/utils.py
+++ DQN/src/utils/utils.py
@@ -3,7 +3,7 @@ from gymnasium.wrappers import RecordVideo
 
 def wrap_env(env: gym.Env, logs_folder: str) -> gym.Env:
     env = RecordVideo(env, 
-                      video_folder=f"{logs_folder}/videos/",
+                      video_folder=f"{logs_folder}",
                       episode_trigger=lambda ep:ep%10 == 0
                       )
     return env
